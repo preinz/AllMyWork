2f2359e51e2c04e7a429fdfddbc48133db65f6b8
prince <nsaaaime@gmail.com>
2020-12-21 18:08:34 +0100

    first commit

---
+++ week-1-master/Day 3/exjsday1.js
@@ -0,0 +1,14 @@
+let addressNumber = 5684;
+let addressStreet = 'Bastos';
+let country = 'Cameroon';
+
+let globalAdress = 'Je vis au '+ addressNumber+ ' ' + addressStreet + ' au ' + country
+console.log(globalAdress)
+
+
+let birthYear = 1992;
+let year = 2025;
+let ageYear = year - birthYear;
+let text = 'I will be ' + ageYear + ' in ' + year + '.'
+console.log(text)
+
