828890f1ad5b209e14a0115636478660661ce4e9
preinz <73167321+preinz@users.noreply.github.com>
2021-03-19 11:42:14 +0100

    send

--- mines/mines/settings.py
+++ mines/mines/settings.py
@@ -74,10 +74,15 @@ WSGI_APPLICATION = 'mines.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mines',
+        'USER': 'postgres',
+        'PASSWORD': 'fossoalix',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
